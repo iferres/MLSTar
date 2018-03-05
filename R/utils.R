8337c09a62d719036eecb4f28edea687e091a104
iferres <igferres@gmail.com>
2018-03-05 10:19:32 -0300

    fixed writing new alleles

--- R/utils.R
+++ R/utils.R
@@ -76,7 +76,7 @@ processResu <- function(resu,
         resu[gp, us[u]] <- nu
         if (write%in%c('new','all')){
           out.newAllele <- paste0(dir, prefix, '/', gene, '.fasta')
-          is <- sub('[.]\\w+$','',rownames(resu)[gp])
+          is <- rownames(resu)[gp]
           sq <- seqinr::read.fasta(out.newAllele, seqtype = 'DNA', as.string = TRUE)
           nsq <- grep(is, names(sq), fixed = TRUE)
           spl <- strsplit(names(sq)[nsq], ';')[[1]]
